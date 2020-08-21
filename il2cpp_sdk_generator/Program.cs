bff8fae6ebf936b8a877cbde8a45e4289db1ff41
yamiM0NSTER <jomex50@gmail.com>
2020-08-21 05:38:22 +0200

    Project init

---
+++ il2cpp_sdk_generator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace il2cpp_sdk_generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
