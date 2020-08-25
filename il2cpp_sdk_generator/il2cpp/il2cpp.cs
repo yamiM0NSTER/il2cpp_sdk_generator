e6157cf58b4e82dc819019c43bb3c1db9af984a7
yamiM0NSTER <jomex50@gmail.com>
2020-08-25 19:00:40 +0200

    github git manager sucks again...

--- il2cpp_sdk_generator/il2cpp/il2cpp.cs
+++ il2cpp_sdk_generator/il2cpp/il2cpp.cs
@@ -7,6 +7,7 @@ namespace il2cpp_sdk_generator
 {
     class il2cpp
     {
+        // Code Registration
         public static Il2CppCodeRegistration64 codeRegistration64 = null;
         public static ulong[] methodPointers; // VA's
         public static ulong[] reversePInvokeWrappers; // VA's
@@ -14,12 +15,24 @@ namespace il2cpp_sdk_generator
         public static ulong[] invokerPointers; // VA's
         public static ulong[] customAttributeGenerators; // VA's
         public static ulong[] unresolvedVirtualCallPointers; // VA's
-        public static Il2CppInteropData[] interopData; // VA's
+        public static Il2CppInteropData[] interopData; // Il2CppInteropData
         public static ulong[] windowsRuntimeFactoryTable; // VA's
 
-
+        // Metadata Registration
         public static Il2CppMetadataRegistration64 metadataRegistration64 = null;
-        
+        public static ulong[] genericClassesPtrs;
+        public static Il2CppGenericClass[] genericClasses;
+        public static ulong[] genericInstsPtrs;
+        public static Il2CppGenericInst[] genericInsts;
+        public static Il2CppGenericMethodFunctionsDefinitions[] genericMethodTable;
+        public static ulong[] typesPtrs;
+        public static Il2CppType[] types;
+        public static Il2CppMethodSpec[] methodSpecs;
+        public static ulong[] fieldOffsetsPtrs;
+        public static Int32[] fieldOffsets;
+        public static ulong[] typeDefinitionsSizesPtrs;
+        public static Il2CppTypeDefinitionSizes[] typeDefinitionsSizes;
+        public static ulong[] metadataUsages;
 
 
     }
